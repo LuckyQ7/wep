92da22bd6c411e1c524372e90f1cfe90da726cb8
lqq <623903979@qq.com>
2024-01-10 10:02:56 +0800

    vue-loader

---
+++ vue-loader/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>{{ message }}</div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const message = ref("ni");
+
+// export default {
+//   data() {
+//     return {
+//       message: "ni",
+//     };
+//   },
+// };
+</script>
+
+<style lang="less">
+#app {
+  background-color: steelblue;
+  div {
+    color: red;
+  }
+}
+</style>
---
+++ vue-loader/webpack.config.js
@@ -0,0 +1,47 @@
+const { VueLoaderPlugin } = require("vue-loader");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    hot: true,
+    open: true,
+  },
+  module: {
+    rules: [
+      { test: /\.vue$/, use: ["vue-loader"] },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      { test: /\.less$/, use: ["style-loader", "css-loader", "less-loader"] },
+      {
+        test: /\.(ts|typescript)$/,
+        loader: "ts-loader",
+        options: {
+          transpileOnly: true,
+          appendTsSuffixTo: [/\.vue$/],
+        },
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      templateContent: `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>vue-loader webpack</title>
+    </head>
+    <body>
+        <div id="app"></div>
+    </body>
+    </html>`,
+    }),
+  ],
+};
